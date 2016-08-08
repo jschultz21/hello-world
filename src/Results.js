c06bbd2ba1addcdffddb2cd2e84fea32179ff1d0
Julia Schultz <julia.schultz91@gmail.com>
2016-08-07 23:22:45 -0400

    ghjobs api working

--- src/Results.js
+++ src/Results.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react'
 
 class Results extends Component {
   render(){
-
     let {jobs}=this.props
     let results=jobs.map((job, index) => {
       return (
         <div key={index}>
-        <p>{job.position_title}</p>
+        <p>{job.title}</p>
+          <p>{job.location}</p>
+            <p>{job.company}</p>
+            <img width="200px" src={job.company_logo} alt={job.company}></img>
+
         </div>
       )
     })
--- src/Utils.js
+++ src/Utils.js
@@ -2,7 +2,7 @@ import $ from "jquery"
 
 export function queryGHJobs (query) {
    var term = query.replace(/\s/, "+"); // replace any white space characters with a "+"
-   var url = "https://api.usa.gov/jobs/search.json?size=100query="+term;
+   var url = "https://jobs.github.com/positions.json?search="+term+"&callback=?";
    // fetch all jobs matching the passed in search as JSON
    return $.getJSON(url).then(function(response) {
      return response
